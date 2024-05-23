84ce25e297f31f1f456d55dd2d3d1daf7ba35353
t0nylombardi <alombardi.331@gmail.com>
2024-05-23 14:16:55 -0400

    solution to code along

---
+++ src/pages/Login.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+const Login = () => {
+  return (
+      <>
+        <header>
+          <NavBar />
+        </header>
+        <main>
+          <h1>Login</h1>
+          <form>
+            <div>
+              <input type="text" name="username" placeholder="Username" />
+            </div>
+            <div>
+              <input type="password" name="password" placeholder="Password" />
+            </div>
+            <input type="submit" value="Submit" />
+          </form>
+        </main>
+      </>
+    );
+}
+
+export default Login
